e1f3655fb5afd2f273b55f95ec2399fec13fb955
Julian Stitz <julian.stitz@googlemail.com>
2013-05-26 00:12:08 +0200

    logging stuff

--- gw2/src/main/java/synchronizer/poolaction/SynchronizeMatchAction.java
+++ gw2/src/main/java/synchronizer/poolaction/SynchronizeMatchAction.java
@@ -3,6 +3,8 @@ package synchronizer.poolaction;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.log4j.Logger;
+
 import model.wvw.IWVWMatch;
 import model.wvw.IWVWModelFactory;
 import utils.InjectionHelper;
@@ -10,14 +12,17 @@ import api.dto.IWVWMatchDTO;
 import api.service.IWVWService;
 
 public class SynchronizeMatchAction extends AbstractMatchIdAction<SynchronizeMatchAction> {
-	private static final transient IWVWService SERVICE = InjectionHelper.INSTANCE.getInjector().getInstance(IWVWService.class);
+	private static final Logger LOGGER = Logger.getLogger(SynchronizeMatchAction.class);
+	private static final IWVWService SERVICE = InjectionHelper.INSTANCE.getInjector().getInstance(IWVWService.class);
 	private static final IWVWModelFactory WVW_MODEL_FACTORY = InjectionHelper.INSTANCE.getInjector().getInstance(IWVWModelFactory.class);
-	
+
 	private static final long serialVersionUID = 8391498327079686666L;
+
 	public SynchronizeMatchAction(List<String> matchIds, int chunkSize) {
 		super(matchIds, chunkSize);
 	}
-	protected SynchronizeMatchAction(List<String> matchIds, int chunkSize, int fromInclusive, int toExclusive){
+
+	protected SynchronizeMatchAction(List<String> matchIds, int chunkSize, int fromInclusive, int toExclusive) {
 		super(matchIds, chunkSize, fromInclusive, toExclusive);
 	}
 
@@ -28,9 +33,10 @@ public class SynchronizeMatchAction extends AbstractMatchIdAction<SynchronizeMat
 
 	@Override
 	protected void perform(String matchId) {
+		LOGGER.trace("Going to synchronize matchId="+matchId);
 		final IWVWMatchDTO matchDto = SERVICE.retrieveMatch(matchId).get();
 		final IWVWMatch match = WVW_MODEL_FACTORY.createMatchBuilder().fromMatchDTO(matchDto, Locale.GERMAN).build();
-		System.out.println(match);
+		LOGGER.debug("Synchronized matchId="+matchId);
 	}
 
 }
