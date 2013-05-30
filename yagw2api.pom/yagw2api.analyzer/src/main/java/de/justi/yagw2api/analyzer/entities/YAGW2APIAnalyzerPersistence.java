0c00c34f4142126eb9ef6e758930dbd95fc3c509
Julian Stitz <julian.stitz@googlemail.com>
2013-05-31 00:49:41 +0200

    organize imports

--- yagw2api.pom/yagw2api.analyzer/src/main/java/de/justi/yagw2api/analyzer/entities/YAGW2APIAnalyzerPersistence.java
+++ yagw2api.pom/yagw2api.analyzer/src/main/java/de/justi/yagw2api/analyzer/entities/YAGW2APIAnalyzerPersistence.java
@@ -7,9 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.eclipse.persistence.sessions.Session;
-import org.eclipse.persistence.sessions.factories.SessionManager;
-
 import de.justi.yagw2api.analyzer.YAGW2APIAnalyzer;
 
 public enum YAGW2APIAnalyzerPersistence {
