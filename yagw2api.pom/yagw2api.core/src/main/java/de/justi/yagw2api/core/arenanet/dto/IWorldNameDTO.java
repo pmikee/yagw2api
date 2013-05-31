cd6ef22ebbaa8472f2317e1f884f02c1a2e52608
Julian Stitz <julian.stitz@googlemail.com>
2013-05-31 15:53:39 +0200

    enhanced world dtos to detect server locale when given

--- yagw2api.pom/yagw2api.core/src/main/java/de/justi/yagw2api/core/arenanet/dto/IWorldNameDTO.java
+++ yagw2api.pom/yagw2api.core/src/main/java/de/justi/yagw2api/core/arenanet/dto/IWorldNameDTO.java
@@ -1,7 +1,14 @@
 package de.justi.yagw2api.core.arenanet.dto;
 
+import java.util.Locale;
+
+import com.google.common.base.Optional;
+
 
 public interface IWorldNameDTO {
 	int getId();
+	
+	String getNameWithoutLocale();
 	String getName();
+	Optional<Locale> getServerLocale();
 }
--- yagw2api.pom/yagw2api.core/src/main/java/de/justi/yagw2api/core/arenanet/dto/impl/WorldNameDTO.java
+++ yagw2api.pom/yagw2api.core/src/main/java/de/justi/yagw2api/core/arenanet/dto/impl/WorldNameDTO.java
@@ -1,19 +1,25 @@
 package de.justi.yagw2api.core.arenanet.dto.impl;
 
 
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.google.common.base.Objects;
+import com.google.common.base.Optional;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.annotations.Since;
 
 import de.justi.yagw2api.core.arenanet.dto.IWorldNameDTO;
 
 class WorldNameDTO implements IWorldNameDTO{
+	private static final transient Pattern PATTERN = Pattern.compile("([^\\[]*)(("+Pattern.quote("[")+"[A-Z]*"+Pattern.quote("]")+")?)");
 	@Since(1.0)
 	@SerializedName("id")
-	private int id;
+	int id;
 	@Since(1.0)
 	@SerializedName("name")
-	private String name;
+	String name;
 	
 	
 	public int getId() {
@@ -25,6 +31,36 @@ class WorldNameDTO implements IWorldNameDTO{
 	}
 	
 	public String toString(){
-		return Objects.toStringHelper(this).add("id", this.id).add("name", this.name).toString();
+		return Objects.toStringHelper(this).add("id", this.id).add("name", this.name).add("locale",this.getServerLocale()).add("nameWithoutLocale", this.getNameWithoutLocale()).toString();
+	}
+
+	@Override
+	public Optional<Locale> getServerLocale() {
+		final Matcher matcher = PATTERN.matcher(this.getName());
+		if(matcher.find()) {
+			if(matcher.group(3)!=null) {
+				String localeTag = matcher.group(3).replaceAll("\\[", "").replaceAll("\\]","").trim().toLowerCase();
+				switch(localeTag) {
+					case "sp":
+						localeTag = "es";
+						break;
+				}
+				return Optional.of(Locale.forLanguageTag(localeTag));
+			}else {
+				return Optional.absent();
+			}
+		}else {
+			return Optional.absent();
+		}
+	}
+
+	@Override
+	public String getNameWithoutLocale() {
+		final Matcher matcher = PATTERN.matcher(this.getName());
+		if(matcher.find()) {
+			return matcher.group(1).trim();
+		}else {
+			return this.getName();
+		}
 	}
 }
