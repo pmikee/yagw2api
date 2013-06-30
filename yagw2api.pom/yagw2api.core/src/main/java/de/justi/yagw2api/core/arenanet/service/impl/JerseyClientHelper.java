9db809417fb739d14066c43719763858b4178a6c
Julian Stitz <julian.stitz@googlemail.com>
2013-06-30 13:37:07 +0200

    On June 20. the GW2 API server switched their SSL certificate which is now signed by GeoTrust. As the GeoTrust root certificate is part of the default Java KeyStore the SSLSocket helper are no longer needed.
    New Version now uses a default SSLSocketFactory for SSLSocket.

--- yagw2api.pom/yagw2api.core/src/main/java/de/justi/yagw2api/core/arenanet/service/impl/JerseyClientHelper.java
+++ yagw2api.pom/yagw2api.core/src/main/java/de/justi/yagw2api/core/arenanet/service/impl/JerseyClientHelper.java
@@ -1,64 +1,29 @@
 package de.justi.yagw2api.core.arenanet.service.impl;
 
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.SSLSocketFactory;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.client.urlconnection.HTTPSProperties;
 
 enum JerseyClientHelper {
 	DEFAULT;
-	
+
 	public static Client createDefaultClient() {
 		return DEFAULT.createClient();
 	}
 
-	private static final int	CONNECTION_TIMEOUT_MILLIS	= 30 * 1000;
-	private static final int	READ_TIMEOUT_MILLIS			= 30 * 1000;
+	private static final int CONNECTION_TIMEOUT_MILLIS = 30 * 1000;
+	private static final int READ_TIMEOUT_MILLIS = 30 * 1000;
 
 	private JerseyClientHelper() {
 
 	}
 
 	private ClientConfig configureClient() {
-		TrustManager[] certs = new TrustManager[] { new X509TrustManager() {
-			public X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
-
-			public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-			}
-
-			public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-			}
-		} };
-		SSLContext ctx = null;
-		try {
-			ctx = SSLContext.getInstance("TLS");
-			ctx.init(null, certs, new SecureRandom());
-		} catch (java.security.GeneralSecurityException ex) {
-		}
-		HttpsURLConnection.setDefaultSSLSocketFactory(ctx.getSocketFactory());
-
-		ClientConfig config = new DefaultClientConfig();
-		try {
-			config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new HTTPSProperties(new HostnameVerifier() {
-				public boolean verify(String hostname, SSLSession session) {
-					return true;
-				}
-			}, ctx));
-		} catch (Exception e) {
-		}
+		HttpsURLConnection.setDefaultSSLSocketFactory((SSLSocketFactory) SSLSocketFactory.getDefault());
+		final ClientConfig config = new DefaultClientConfig();
 		return config;
 	}
 
