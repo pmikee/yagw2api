d84ab358e10051a690d520de2efeedbdf04f8f92
maik <maik@maik-XPS-M1530>
2015-06-04 13:45:11 +0200

    exlude css and js. use own tiles service instead of guildwars tile service

--- yagw2api.pom/yagw2api.server/src/main/resources/static/js/mainJS.js
+++ yagw2api.pom/yagw2api.server/src/main/resources/static/js/mainJS.js
@@ -1,28 +1,27 @@
 (function() {
-	"use strict";
+		"use strict";
 
-	var map, southWest, northEast;
+		var map, southWest, northEast;
 
-	function unproject(coord) {
-	    return map.unproject(coord, map.getMaxZoom());
-	}
+		function unproject(coord) {
+		    return map.unproject(coord, map.getMaxZoom());
+		}
 
-	map = L.map("map", {
-	  minZoom: 0,
-	  maxZoom: 7,
-	  crs: L.CRS.Simple
-	}).setView([0, 0], 0);
+		map = L.map("map", {
+		  minZoom: 0,
+		  maxZoom: 7,
+		  crs: L.CRS.Simple
+		}).setView([0, 0], 0);
 
-	southWest = unproject([0, 32768]);
-	northEast = unproject([32768, 0]);
+		southWest = unproject([0, 32768]);
+		northEast = unproject([32768, 0]);
 
-	map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
+		map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
+		  L.tileLayer(window.location.origin+"/map/tile/1/1/{z}/{x}/{y}.jpg", {
+		  minZoom: 0,
+		  maxZoom: 7,
+		  continuousWorld: true,
+		  subdomains: [1, 2, 3, 4 ]
+		}).addTo(map);
 
-	L.tileLayer("https://tiles{s}.guildwars2.com/1/1/{z}/{x}/{y}.jpg", {
-	  minZoom: 0,
-	  maxZoom: 7,
-	  continuousWorld: true,
-	  subdomains: [1, 2, 3, 4 ]
-	}).addTo(map);
-
-}());
+		}());
--- yagw2api.pom/yagw2api.server/src/main/webapp/WEB-INF/jsp/map.jsp
+++ yagw2api.pom/yagw2api.server/src/main/webapp/WEB-INF/jsp/map.jsp
@@ -1,6 +1,9 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
+	<title>MapOverview</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css" />
    <link rel="stylesheet" type="text/css" href="../css/mainStyle.css" media="all" />
 </head>
