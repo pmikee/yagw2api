6b6d90460f96c4d65f11dd6b44d16bb1212d6f4e
Julian Stitz <julian.stitz@googlemail.com>
2015-06-05 22:48:37 +0200

    excluded jackson from classpath; configured pretty print for gson

--- yagw2api.pom/yagw2api.server/src/main/java/yagw2api/server/ServerMain.java
+++ yagw2api.pom/yagw2api.server/src/main/java/yagw2api/server/ServerMain.java
@@ -22,9 +22,18 @@ package yagw2api.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @SpringBootApplication
 public class ServerMain {
+
+	@Bean
+	public Gson gson() {
+		return new GsonBuilder().setPrettyPrinting().create();
+	}
 
 	// STATICS
 	public static final void main(final String[] args) {
